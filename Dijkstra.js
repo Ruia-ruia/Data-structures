86090f465a6bdf35089f39159c0688664d5de8eb
Tuwhakatere <61450241+Tuwhakatere@users.noreply.github.com>
2020-02-26 15:09:58 +1100

    Update Dijkstra.js

--- Dijkstra.js
+++ Dijkstra.js
@@ -11,8 +11,6 @@ class PQueue {
   }
 
   set(elem, p){
-    //if (typeof elem != 'string') return -1;
-    //else if (typeof p != 'number') return -1;
 
     var qitem = new QElem(elem, p);
     var flag = false;
@@ -45,7 +43,7 @@ class PQueue {
   }
 }
 
-var pQ = new PQueue(); //set(el, p), unset(), get(index), size()
+var pQ = new PQueue(); //available methods: set(elem, priority), unset(), get(index), size()
 
 class Edge {
   constructor(start, end, weight){
@@ -101,21 +99,61 @@ var nodes = [
   {B:0, A:2, D:7, F:5, H:8},
   {C:0, A:3}
 ]
-
 var graph1 = new Graph(nodes);
 graph1.setup();
 
-function qNodes(){
-  var init = pQ.set(graph1.vertices[0], 0);
-
-  for(i = 1; i < nodes.length; i++){
-    pQ.set(graph1.vertices[i], 100);
+/*
+key := the name of the vertex
+0. get the offset in nodes where the key has value 0
+1. return the object at the same offset in the graph object
+*/
+function getOffsetFromKey(key){
+  for(i in nodes){
+    if(nodes[i][key] === 0){
+      return graph1.vertices[i];
+    }
   }
 }
 
-qNodes();
+/*
+p := priority
+0. set up pQ with Graph data (vertices, their edges [start, end, weight], and p-distance)
+1. set first element to distance p-0
+2. set all others to p-1000, where 1000 stands for infinity (see: Dijkstra specification)
+*/
+(function qNodes(){
+  pQ.set(graph1.vertices[0], 0);
 
-console.log(pQ.items);
+  for(i = 1; i < nodes.length; i++){
+    pQ.set(graph1.vertices[i], 1000);
+  }
+})();
+
+function findShortestPath(){
+  while(pQ.size() !== 0){
+    var qElem = pQ.unset();
+    var edges = qElem.elem.getEdges();
+
+    for(i in edges){
+      var edge = edges[i];
+      var destinationKey = edge.getTermo();
+      var destinationObject = getOffsetFromKey(destinationKey);
+      var newDistance = edge.getWeight() + qElem.p;
+
+    //  console.log(edge);
+      //console.log(destinationKey);
+      console.log(destinationObject);
+    //  console.log(newDistance);
+//      if(newDistance < destinationObject.){
+
+//      }
+    }
+
+    break;
+  }
+};
+
+findShortestPath();
  
 
 
