55af92e51f7a4c63d371d6c703b97099b6a2d051
Tuwhakatere <61450241+Tuwhakatere@users.noreply.github.com>
2020-03-03 16:44:44 +1300

    Update Dijkstra.js

--- Dijkstra.js
+++ Dijkstra.js
@@ -1,3 +1,6 @@
+/*
+data structures including priority queue and its element structure, alongwith a graph class for instantiating weighted edges
+*/
 class QElem {
   constructor(elem, p){
     this.elem = elem;
@@ -28,6 +31,7 @@ class PQueue {
     }
   }
 
+
   unset(){
      if(this.items.length !== 0){
        return this.items.shift();
@@ -43,111 +47,68 @@ class PQueue {
   }
 }
 
-var pQ = new PQueue(); //available methods: set(elem, priority), unset(), get(index), size()
-
-class Edge {
-  constructor(start, end, weight){
-    this.start = start;
-    this.end = end;
-    this.weight = weight;
-  }
-
-  getWeight(){
-    return this.weight;
-  }
-
-  getTermo(){
-    return this.end;
-  }
-}
-
-class Vertex {
-  constructor(nodeDict){
-    this.nodekeys = Object.keys(nodeDict);
-    this.nodevals = Object.values(nodeDict);
-    this.edges = [];
-  }
-  setup() {
-    for (var i = 1; i < this.nodevals.length; i++){
-      const newEdge = new Edge(this.nodekeys[0], this.nodekeys[i], this.nodevals[i])
-    this.edges.push(newEdge)
-    }
-  }
-
-  getEdges(){
-    return this.edges;
-  }
-}
-
 class Graph {
-  constructor(nodes){
-    this.nodes = nodes;
-    this.vertices = [];
-  }
+   constructor(){
+       this.nodes = {}
+   }
+   
+   initNode(node){
+       this.nodes[node] = [];
+   }
 
-  setup(){
-    for (var i = 0; i < this.nodes.length; i++){
-      const vertexObject = new Vertex(this.nodes[i]);
-      this.vertices.push(vertexObject)
-      vertexObject.setup();
-      }
-    }
+   initEdge(node1, node2, work){
+this.nodes[node1].push({[node2]:work})
+this.nodes[node2].push({[node1]:work})
+   }
 }
-
-var nodes = [
-  {A:0, B:2, C:3},
-  {B:0, A:2, C:4, D:7},
-  {C:0, A:3, B:4},
-  {D:0, B:7}
-]
-var graph1 = new Graph(nodes);
-graph1.setup();
 
 /*
-key := the name of the vertex
-0. get the offset in nodes where the key has value 0
-1. return the object at the same offset in the graph object
+main computations - discernment of shortest possible paths between startNode and all other nodes
 */
-function getOffsetFromKey(key){
-  for(i in nodes){
-    if(nodes[i][key] === 0){
-      return graph1.vertices[i];
-    }
-  }
-}
+function djik(){
+   dists = {};
+   pQ.set(entryNode, 0);
+   backingStore = {};
 
-startNode = 'A';
-var distances = {};
-
-for(i = 0; i < nodes.length; i++){
-  node = nodes[i];
-  if(Object.keys(node)[0] === startNode) { distances[Object.keys(node)[0]] = 0; }
-  else { distances[Object.keys(node)[0]] = 1000;
- }
-}
-pQ.set(graph1.vertices[0], 0);
-
-(function Djik(){
-  while(!(pQ.size() === 0)){
-    let cVertex = pQ.unset();
-    let weight = cVertex.p;
-    let edgeList = cVertex.elem.nodekeys;
-    let edgeObjectList = [];      
-
-    edgeList.forEach(name => {
-edgeObjectList.push(getOffsetFromKey(name))
-    });
-    
-    edgeObjectList.forEach(adj => {
-       let sum = distances[edgeList[0]] + adj.nodevals[1];
-
-       if(sum < distances[adj.nodekeys[0]]){
-         distances[adj.nodekeys[0]] = sum;
-         pQ.set(getOffsetFromKey(adj.nodekeys[0]), sum);
+   dists[entryNode] = 0;
+   var keys = Object.keys(g.nodes)
+   keys.forEach(adj => {
+       if(!(adj == entryNode)){
+          dists[adj] = 10000;
        }
-    });
-  }
-})();
+   });
+  
+   while(!(pQ.size() == 0)){
+      var currentVertex = pQ.unset(entryNode);
+      var currdist = currentVertex.p;
+
+      g.nodes[currentVertex.elem].forEach(adj => {
+         let adjKey = Object.keys(adj)[0];
+         let adjWork = Object.values(adj)[0]
+         let cmpWork = dists[currentVertex.elem] + adjWork;
 
 
-console.log(distances);
+         if(cmpWork < dists[adjKey]){
+             dists[adjKey] = cmpWork;
+             backingStore[adjKey] = cmpWork;
+             pQ.set(adjKey, cmpWork);
+         }
+      });
+   }
+  return backingStore;
+}
+
+
+/*
+Function invocations and logging results
+*/
+var pQ = new PQueue();
+var g = new Graph();
+g.initNode('A');
+g.initNode('B');
+g.initNode('C');
+g.initEdge('A', 'B', 3);
+g.initEdge('B', 'C', 5);
+
+var entryNode = 'A';
+console.log(djik());
