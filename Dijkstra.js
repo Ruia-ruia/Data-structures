68f901f7a0a8af5eb210d5ba273ef20ff65dab66
Ruia-ruia <61450241+Ruia-ruia@users.noreply.github.com>
2020-07-10 18:46:10 +1000

    Update Dijkstra.js

--- Dijkstra.js
+++ Dijkstra.js
@@ -1,235 +1,114 @@
-#include <iostream>
-#include <vector>
-#include <ctime>
-
-using std::cout;
-using std::cin;
-using std::endl;
-using std::vector;
-
-//size of graph, max weight of edges
-const int size = 10;
-const int max_weight = 100;
-
-class node;
-class graph;
-class priorityq;
-class qelem;
-
-class qelem {
-  node* item;
-  int p;
-
-  public:
-    qelem(node* it, int priority) {
-        item = it;
-        p = priority;
-    }
-
-    node* get_item() { return item; }
-    int get_priority() { return p; }
-};
-
-class priorityq {
-  public:
-    void set_item(node& n, int p) {
-        qelem elem(&n, p);
-        bool flag = false;
-
-        for (int i = 0; i < items.size(); ++i) {
-            if(items[i].get_priority() > elem.get_priority()) {
-                items.insert(items.begin() + i, elem);
-                flag = true;
-                break;
-            }
-        }
-
-        if (!flag) {
-            items.push_back(elem);
-        }
-    }
-
-    qelem return_head() {
-        qelem n = items[0];
-        items.erase(items.begin());
-        return n;
-    }
-
-    int get_size() { return items.size(); }
-
-    void print_items() {
-        for (int i = 0; i < items.size(); ++i) {
-            cout << items[i].get_item() << endl;
-            cout << items[i].get_priority() << endl;
-            cout << "-----------" << endl;
-        }
-    }
-
-  private:
-    vector<qelem> items;
-};
-
-class node {
-  public:
-    node(int id):id(id){};
-
-    void print_neighbours();
-
-    void add_neighbour(int neigh, int weight) {
-      vector<int> v;
-      v.push_back(neigh);
-      v.push_back(weight);
-      neighbours.push_back(v); };
-
-    int get_weight(int neighbour_id) {
-        for (int i = 0; i < neighbours.size(); ++i) {
-            if (neighbours[i][0] == neighbour_id) {
-                return neighbours[i][1];
-            }
-        }
-        return -1;
-    }
-
-    vector<vector<int> >& get_neighbours() {
-        return neighbours;
-    }
-
-    int get_id() { return id; }
-
-  private:
-    int id;
-    vector< vector<int> > neighbours;
-};
-void node::print_neighbours() {
-    for (int i = 0; i < neighbours.size(); ++i) {
-        cout << "neighbour: " << neighbours[i][0] <<
-                 "| weight: " << neighbours[i][1] << endl;
-    }
+/*
+data structures including priority queue and its element structure, alongwith a graph class for instantiating weighted edges
+*/
+class QElem {
+  constructor(elem, p){
+    this.elem = elem;
+    this.p = p;
+  }
 }
 
-class graph {
-  public:
-    void add_node(int id) {
-      nodes.push_back(node(id)); }
+class PQueue {
+  constructor(){
+    this.items = [];
+  }
 
-    void add_edge(int id1, int id2, int weight) {
-      nodes[id1].add_neighbour(id2, weight);
-        nodes[id2].add_neighbour(id1, weight); }
+  set(elem, p){
 
-    void print_neighbours_of(int id) {
-      nodes[id].print_neighbours(); }
+    var qitem = new QElem(elem, p);
+    var flag = false;
 
-    int get_weight(int id, int neighbour_id) {
-        return nodes[id].get_weight(neighbour_id);
+    for(var i = 0; i < this.items.length; i++){
+      if(this.items[i].p > qitem.p){
+        this.items.splice(i, 0, qitem);
+        flag = true;
+        break;
+      }
     }
 
-    node& resolve_node(int id) {
-        return nodes[id];
+    if(!flag){
+      this.items.push(qitem);
     }
+  }
 
-    vector<int> dijkstra();
 
-  private:
-    vector<node> nodes;
-};
-vector<int> graph::dijkstra() {
-    vector<int> backtrace;
-    vector<int> dists;
-    priorityq pq;
-    int curr = 0;
+  unset(){
+     if(this.items.length !== 0){
+       return this.items.shift();
+     } else console.log("Empty.");
+   }
 
-    //set up
-    dists.resize(size);
-    backtrace.resize(size);
-    for (int i = 0; i < backtrace.size(); ++i) { backtrace[i] = -1; }
-    backtrace[0] = 0;
-    
-    pq.set_item(resolve_node(curr), 0);
+  get(index){
+    return this.items[index];
+  }
 
-    dists[curr] = 0;
-    for (int i = 1; i < nodes.size(); ++i) {
-        dists[i] = 10000;     //effective infinity
-    }
-
-    //main loop
-    while (pq.get_size()) {
-        qelem n = pq.return_head();
-        node *currnode = n.get_item();
-        int currdist = n.get_priority();
-        vector<vector<int> > d = currnode -> get_neighbours();
-
-        for (int i = 0; i < d.size(); ++i) {
-            int node = d[i][0];
-            int weight = d[i][1];
-            int curr_key = n.get_item() -> get_id();
-            int adj_key = node;
-            vector<int> v;
-
-            int cmp_weight = dists[curr_key] + weight;
-
-            if (cmp_weight < dists[adj_key]) {
-                dists[adj_key] = cmp_weight;
-                backtrace[adj_key] = cmp_weight;
-                pq.set_item(resolve_node(adj_key), cmp_weight);
-            }
-        }
-    }
-
-    return backtrace;
+  size(){
+   return this.items.length;
+  }
 }
 
-//Generate random graph from reference
-void gen_graph(graph& new_g) {
-    bool** matrix;
-    srand(time(0));
+class Graph {
+   constructor(){
+       this.nodes = {}
+   }
+   
+   initNode(node){
+       this.nodes[node] = [];
+   }
 
-    //allocate space for 2D matrix
-    matrix = new bool * [size];
-    for (int i = 0; i < size; ++i) {
-        matrix[i] = new bool[size];
-    }
-
-    //initialise boolean connectivity matrix
-    for (int i = 0; i < size; ++i) {
-        for (int j = i; j < size; ++j) {
-            if (i == j) matrix[i][j] = false;
-            else matrix[i][j] =
-                 matrix[j][i] = ((static_cast<float>(rand())
-                              / static_cast<float>(RAND_MAX))
-                              < 0.19);
-        }
-    }
-
-    //generate actual graph with nodes
-    //each node with an adjacency list
-    for (int i = 0; i < size; ++i) {
-        new_g.add_node(i);
-    }
-
-    srand(time(0));
-    for (int i = 0; i < size; ++i) {
-        for (int j = i; j < size; ++j) {
-            if (matrix[i][j]) {
-                int weight = 1 + (static_cast<int>(rand()) % max_weight);
-                new_g.add_edge(i, j, weight);
-
-            }
-        }
-    }
-
-    //garbage collection
-    for (int i = 0; i < size; ++i) delete matrix[i];
-    delete [] matrix;
+   initEdge(node1, node2, work){
+       this.nodes[node1].push({[node2]:work})
+       this.nodes[node2].push({[node1]:work})
+   }
 }
 
-int main() {
-    graph g;
-    gen_graph(g);
+/*
+main computations - discernment of shortest possible paths between entryNode (e.g. 'A') and all other nodes
+*/
+function dijkstra(){
+   dists = {};
+   pQ.set(entryNode, 0);
+   backingStore = {};
 
-    vector<int> res = g.dijkstra();
-    for (int i = 0; i < res.size(); ++i) {
-        cout << i << ": " << res[i] << endl;
-    }
+   dists[entryNode] = 0;
+   var keys = Object.keys(g.nodes)
+   keys.forEach(adj => {
+       if(!(adj == entryNode)){
+          dists[adj] = 10000;
+       }
+   });
+  
+   while(!(pQ.size() == 0)){
+      var currentVertex = pQ.unset(entryNode);
+      var currdist = currentVertex.p;
 
+      g.nodes[currentVertex.elem].forEach(adj => {
+         let adjKey = Object.keys(adj)[0];
+         let adjWork = Object.values(adj)[0]
+         let cmpWork = dists[currentVertex.elem] + adjWork;
+
+
+         if(cmpWork < dists[adjKey]){
+             dists[adjKey] = cmpWork;
+             backingStore[adjKey] = cmpWork;
+             pQ.set(adjKey, cmpWork);
+         }
+      });
+   }
+  return backingStore;
 }
 
+
+/*
+Function invocations and logging results
+*/
+var pQ = new PQueue();
+var g = new Graph();
+g.initNode('A');
+g.initNode('B');
+g.initNode('C');
+g.initEdge('A', 'B', 3);
+g.initEdge('B', 'C', 5);
+
+var entryNode = 'A';
+console.log(dijkstra());
